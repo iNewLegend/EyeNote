abea631ff0b8c27039dbdb6b6bdddb966f5a2d9d
Leonid Vinikov <czf.leo123@gmail.com>
2025-10-29 13:48:49 +0200

    Fix: (`workspace`) - Ensures environment file exists
    Checks for the existence of the .env file at the project root
    and throws an error if it doesn't exist, preventing runtime issues.
    Also, outputs client and redirect URLs to the console.

--- packages/workspace/src/vite/config.ts
+++ packages/workspace/src/vite/config.ts
@@ -1,3 +1,4 @@
+import * as fs from "node:fs";
 import { fileURLToPath } from "node:url";
 import { resolve } from "path";
 import { config as loadEnv } from "dotenv";
@@ -5,9 +6,15 @@ import { defineConfig } from "vite";
 
 type TReturn = ReturnType<typeof defineConfig>;
 
-const currentPath = fileURLToPath( import.meta.url )
+const currentPath = fileURLToPath( import.meta.url ),
+    envPath = resolve( currentPath, "..", "..", "..", "..", "..", ".env" );
 
-loadEnv( { path: resolve( currentPath, "..", "..", ".env" ) } );
+// Check env path exists
+if ( !fs.existsSync( envPath ) ) {
+    throw new Error( `Environment file not found at: ${ envPath }` )
+}
+
+loadEnv( { path:  envPath } );
 
 export function getBaseConfig ( { hostEnvPrefix } : { hostEnvPrefix ?: string } ) : TReturn {
     const googleClientId =
@@ -27,7 +34,10 @@ export function getBaseConfig ( { hostEnvPrefix } : { hostEnvPrefix ?: string } 
         );
     }
 
-    const reuslt = {
+    console.log( "[EyeNote] Using GOOGLE_CLIENT_ID:", googleClientId );
+    console.log( "[EyeNote] Using GOOGLE_REDIRECT_URI:", googleRedirectUri );
+
+    const result = {
         define: {
             "process.env.NODE_ENV": JSON.stringify( process.env.NODE_ENV ?? "development" ),
             "import.meta.env.VITE_GOOGLE_CLIENT_ID": JSON.stringify( googleClientId ),
@@ -40,7 +50,7 @@ export function getBaseConfig ( { hostEnvPrefix } : { hostEnvPrefix ?: string } 
             port = Number.parseInt( process.env[ `${ hostEnvPrefix }_PORT` ] ?? "5173", 10 );
 
         return {
-            ... reuslt,
+            ... result,
             server: {
                 host,
                 port,
@@ -48,5 +58,5 @@ export function getBaseConfig ( { hostEnvPrefix } : { hostEnvPrefix ?: string } 
         };
     }
 
-    return reuslt;
+    return result;
 }
